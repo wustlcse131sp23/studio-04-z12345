7499acfe70ca939281fab7ce0511c4e1a4f13c8c
10370 <10370@TABLET-O13EIHM1.dhcp.wustl.edu>
2023-02-23 11:43:26 -0600

    123

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,15 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shapeType=in.next();
+		int redComponent=in.nextInt();
+		int greenComponent=in.nextInt();
+		int blueComponent=in.nextInt();
+		boolean isfilled=in.nextBoolean();
+		double parametOne=in.nextDouble();
+		double parametTwo=in.nextDouble();
+		double parametThree=in.nextDouble();
+		double parametFour=in.nextDouble();
+		StdDraw.rectangle(parametOne, parametTwo, parametThree, parametFour);
 	}
 }
